eb7418fac8aa1abbec15aaedb41403fb178221a6
Antonio Villar <dyavr6@gmail.com>
2025-08-16 15:23:38 +0000

    que en el Prom. Atención / Día no se muesten los decimales

--- src/app/(main)/percentages/page.tsx
+++ src/app/(main)/percentages/page.tsx
@@ -276,7 +276,7 @@ export default function PercentagesPage() {
   const formatMinutesToHoursAndMinutes = (minutes: number): string => {
     if (minutes <= 0) return '0h 0m';
     const hours = Math.floor(minutes / 60);
-    const remainingMinutes = minutes % 60;
+    const remainingMinutes = Math.floor(minutes % 60);
     return `${hours}h ${remainingMinutes}m`;
   };
 
